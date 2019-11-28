9f772be15847a0c9102b20001b2c4aa3a066edc6
amandabacelli <amanda.sted@gmail.com>
2019-11-27 21:40:59 -0300

    iniciando o cursocontroller

--- src/controllers/cursoController.js
+++ src/controllers/cursoController.js
@@ -1,2 +1,15 @@
 const Cursos = require("../models/cursoSchema")
 
+exports.post = (req,res) => {
+    let curso = new Cursos(req.body)
+    curso.save(function(err){
+        if(err) {
+            return res.status(500).send(err)
+        }
+        return res.status(201).send({
+            status: "true",
+            mensagem: "Curso cadastrado com sucesso"
+        })
+    })
+}
+
